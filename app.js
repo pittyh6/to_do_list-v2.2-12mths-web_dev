5d292a5c855be07e5edab24263c66a12b1e646b4
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-05-16 07:43:25 +1000

    app.js file. Set server, express, ejs views, bodyparser

---
+++ app.js
@@ -0,0 +1,25 @@
+/*jshint esversion: 6 */
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+//set the view Engine to EJS
+app.set('views','./views')
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+//Server Static Files
+app.use(express.static('public'))
+
+//Define routes
+app.get('/',function(req,res){
+    res.sendFile(__dirname + 'index.html')
+})
+
+//start server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
