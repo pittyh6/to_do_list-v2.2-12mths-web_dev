fbd19c327627af3e2af92a0f539aefc057fe1ef6
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-05-27 17:54:14 +1000

    create function hide_show. Create eventListenet tfor nav_list and create new list to show and to hide

--- public/js/index.js
+++ public/js/index.js
@@ -6,3 +6,25 @@ let btn_create_new_list = $(".btn-create-new-list")
 let btn_download_list = $(".btn-download-list")
 let btn_create = $(".btn-create")
 
+//hide elements
+let nav_list_group = $(".nav-list-group")
+let field_create_list = $(".field-create-list")
+
+
+function hide_show(elementClicked){
+    if(elementClicked.hasClass("invisible")){
+        elementClicked.removeClass("invisible")
+        elementClicked.addClass("visible")
+    }else{
+        elementClicked.removeClass("visible")
+        elementClicked.addClass("invisible")
+    }
+}
+
+
+nav_lists[0].addEventListener("click",function(){
+    hide_show(nav_list_group)
+})
+btn_create_new_list[0].addEventListener('click', function(){
+    hide_show(field_create_list)
+})
