eda57fdf698184e8f047c290fb9af2590a2b026f
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-05-27 17:43:07 +1000

    creating variables for all buttons on the page

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,8 @@
 // buttons
+let nav_lists = $(".nav-lists")
+let btn_delete = $(".btn-delete")
+let btn_add = $(".btn-add")
+let btn_create_new_list = $(".btn-create-new-list")
+let btn_download_list = $(".btn-download-list")
+let btn_create = $(".btn-create")
+
