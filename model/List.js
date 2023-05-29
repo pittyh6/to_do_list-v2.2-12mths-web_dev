d42646043d348fc65872a9c89e7b5e7ed7ba5f8c
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-05-29 10:11:51 +1000

    create the schema

--- model/List.js
+++ model/List.js
@@ -1,1 +1,10 @@
 import mongoose from 'mongoose';
+const {Schema, model} = mongoose;
+
+//Create the schema for listdb
+const listSchema = new Schema({
+    name_list: String,
+    items:[{
+        item_name: String,
+    }]
+})
