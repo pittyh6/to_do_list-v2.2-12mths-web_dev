8190f0380c26b4f7a3ebb5e97402011acd3a6d7e
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-06-11 13:37:29 +1000

    create new function findListClickedOnDB(textElementClicked) to do the bd find elements on the list

--- public/js/script.js
+++ public/js/script.js
@@ -9,15 +9,21 @@ function hide_show(elementClicked) {
     }
 }
 
-
+// to get the element which contains an specific class passed by parameter
 function getClickedElementPage(element) {
     window.onclick = e => {
         textElementClicked = e.target.innerText
         if ($(e.target).hasClass(element) == true) {
             console.log(textElementClicked)
+            findListClickedOnDB(textElementClicked)
             return textElementClicked
         } else {
             console.log("doesn't have the class/element ", element)
         }
     }
+}
+
+// find the element clicked inside the db
+function findListClickedOnDB(textElementClicked){
+    console.log("findListClickedOnDB ",textElementClicked)
 }
